aeb06c42e5840f0ec6f6091771fe60c60b47d526
Anthony DeMatteo <adematteo94@gmail.com>
2017-05-16 18:54:57 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+console.log("Up and running!");
+var card1 = "Joker";
+var card2 = "Joker";
+var card3 = "Ace";
+var card4 = "Ace";
+var card5 = "Jack";
+var card6 = "Jack";
+var card7 = "Queen";
+var card8 = "Queen";
+var card9 = "King";
+var card10 = "King";
+var card11 = "Blank";
+var card12 = "Blank";
+
+console.log("User flipped" + card1);
+console.log("User flipped" + card2);
+
+
+
+
